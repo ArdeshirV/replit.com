c227138c31956c7d43660de72996045eac2eb4bc
Ardeshir <e-job@protonmail.com>
2023-08-13 22:22:42 +0330

    Modify by vscode.dev

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def main()-> int:
+    print('The Python Programming Language')
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+
+
+def say_hello():
+    pass
+
+def merge_sort():
+    pass
+
+
+def say_hello():
+    
